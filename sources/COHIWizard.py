010ad9195b3a5d140dda89af9b0a153d34884658
scharfetter <hermann.scharfetter@tugraz.at>
2025-12-30 11:23:36 +0100

    newest requirements.txt

--- sources/COHIWizard.py
+++ sources/COHIWizard.py
@@ -353,7 +353,7 @@ class PdfViewer_old(QWidget):
 
 
 from PyQt5.QtWidgets import QWidget, QVBoxLayout
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+#from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 from pathlib import Path
 
